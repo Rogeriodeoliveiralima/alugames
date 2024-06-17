3e43f5ab2159569b6c259a7286ba755ea6c4a006
Rogerio Lima <rlimapg@gmail.com>
2024-06-17 17:39:09 -0300

    relembrendo alugagames

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 
+
 function alterarStatus(id){
-   let gameClicado = document.getElementById(`game-${id}`); 
+let alugaGames = document.getElementById(`game-${id}`);
+let imagem = alugaGames.querySelector('.dashboard__item__img');
+let botao = alugaGames.querySelector('.dashboard__item__button');
+let nome= alugaGames.querySelector('.dashboard__item__name');
 
-   let imagem = gameClicado.querySelector('.dashboard__item__img');
-   let botao = gameClicado.querySelector('.dashboard__item__button');
-
-    if(imagem.classList.contains('dashboard__item__img--rented')){
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent ='Alugar';
-    }else{
-        imagem.classList.add('dashboard__item__img--rented');
-        botao.classList.add('dashboard__item__button--return');
-        botao.textContent ='Devolver';
-    }
+if( imagem.classList.contains('dashboard__item__img--rented')){
+    imagem.classList.remove('dashboard__item__img--rented');
+    botao.classList.remove('dashboard__item__button--return');
+    botao.textContent = 'Alugar';
+}else{
+    imagem.classList.add('dashboard__item__img--rented');
+    botao.classList.add('dashboard__item__button--return');
+    botao.textContent = 'Devolver';
 }
 
+}
